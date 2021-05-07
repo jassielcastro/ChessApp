091bb68d069e98cbdd813773544b3a470b31427c
Alan Jassiel Castro Melgoza <jassmel05@gmail.com>
2021-05-07 10:29:40 -0500

    Replaced all pieces object for sealed class (#1)

--- domain/src/main/java/com/ajcm/domain/game/Movement.kt
+++ domain/src/main/java/com/ajcm/domain/game/Movement.kt
@@ -1,11 +1,11 @@
 package com.ajcm.domain.game
 
 import com.ajcm.domain.board.Position
-import com.ajcm.domain.pieces.ChessPiece
+import com.ajcm.domain.pieces.Piece
 
-typealias Removed = Pair<ChessPiece, Position>
+typealias Removed = Pair<Piece, Position>
 
-class Movement(val chessPiece: ChessPiece) {
+class Movement(val piece: Piece) {
     lateinit var position: Position
     var removedEnemy: Removed? = null
 }
--- domain/src/main/java/com/ajcm/domain/players/Player.kt
+++ domain/src/main/java/com/ajcm/domain/players/Player.kt
@@ -7,22 +7,22 @@ import com.ajcm.domain.pieces.*
 data class Player(val color: Color, var isMoving: Boolean) {
     val movesMade: MutableList<Movement> = mutableListOf()
 
-    val availablePieces: MutableList<ChessPiece> = mutableListOf(
-        Pawn(if (color == Color.WHITE) Pair(1, 2) else Pair(1, 7) , color),
-        Pawn(if (color == Color.WHITE) Pair(2, 2) else Pair(2, 7) , color),
-        Pawn(if (color == Color.WHITE) Pair(3, 2) else Pair(3, 7) , color),
-        Pawn(if (color == Color.WHITE) Pair(4, 2) else Pair(4, 7) , color),
-        Pawn(if (color == Color.WHITE) Pair(5, 2) else Pair(5, 7) , color),
-        Pawn(if (color == Color.WHITE) Pair(6, 2) else Pair(6, 7) , color),
-        Pawn(if (color == Color.WHITE) Pair(7, 2) else Pair(7, 7) , color),
-        Pawn(if (color == Color.WHITE) Pair(8, 2) else Pair(8, 7) , color),
-        Bishop(if (color == Color.WHITE) Pair(3, 1) else Pair(3, 8) , color),
-        Bishop(if (color == Color.WHITE) Pair(6, 1) else Pair(6, 8) , color),
-        Knight(if (color == Color.WHITE) Pair(2, 1) else Pair(2, 8) , color),
-        Knight(if (color == Color.WHITE) Pair(7, 1) else Pair(7, 8) , color),
-        Rook(if (color == Color.WHITE) Pair(1, 1) else Pair(1, 8) , color),
-        Rook(if (color == Color.WHITE) Pair(8, 1) else Pair(8, 8) , color),
-        Queen(if (color == Color.WHITE) Pair(4, 1) else Pair(4, 8) , color),
-        King(if (color == Color.WHITE) Pair(5, 1) else Pair(5, 8) , color)
+    val availablePieces: MutableList<Piece> = mutableListOf(
+        Piece.Pawn(if (color == Color.WHITE) Pair(1, 2) else Pair(1, 7) , color),
+        Piece.Pawn(if (color == Color.WHITE) Pair(2, 2) else Pair(2, 7) , color),
+        Piece.Pawn(if (color == Color.WHITE) Pair(3, 2) else Pair(3, 7) , color),
+        Piece.Pawn(if (color == Color.WHITE) Pair(4, 2) else Pair(4, 7) , color),
+        Piece.Pawn(if (color == Color.WHITE) Pair(5, 2) else Pair(5, 7) , color),
+        Piece.Pawn(if (color == Color.WHITE) Pair(6, 2) else Pair(6, 7) , color),
+        Piece.Pawn(if (color == Color.WHITE) Pair(7, 2) else Pair(7, 7) , color),
+        Piece.Pawn(if (color == Color.WHITE) Pair(8, 2) else Pair(8, 7) , color),
+        Piece.Bishop(if (color == Color.WHITE) Pair(3, 1) else Pair(3, 8) , color),
+        Piece.Bishop(if (color == Color.WHITE) Pair(6, 1) else Pair(6, 8) , color),
+        Piece.Knight(if (color == Color.WHITE) Pair(2, 1) else Pair(2, 8) , color),
+        Piece.Knight(if (color == Color.WHITE) Pair(7, 1) else Pair(7, 8) , color),
+        Piece.Rook(if (color == Color.WHITE) Pair(1, 1) else Pair(1, 8) , color),
+        Piece.Rook(if (color == Color.WHITE) Pair(8, 1) else Pair(8, 8) , color),
+        Piece.Queen(if (color == Color.WHITE) Pair(4, 1) else Pair(4, 8) , color),
+        Piece.King(if (color == Color.WHITE) Pair(5, 1) else Pair(5, 8) , color)
     )
 }
