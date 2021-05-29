8c7e57a5c62811177912963c15e7c64244ac27a1
Jassiel <jassmel05@gmail.com>
2021-05-29 10:02:35 -0500

    Background color added for each view

--- design/src/main/java/com/ajcm/design/archi/BaseFragment.kt
+++ design/src/main/java/com/ajcm/design/archi/BaseFragment.kt
@@ -1,9 +1,12 @@
 package com.ajcm.design.archi
 
 import android.os.Bundle
+import android.view.View
 import androidx.annotation.LayoutRes
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import com.ajcm.design.R
 
 abstract class BaseFragment<UI: UiState, AS : ActionState, VM: ScopedViewModel<UI, AS>>(@LayoutRes layout: Int): Fragment(layout) {
 
@@ -13,6 +16,13 @@ abstract class BaseFragment<UI: UiState, AS : ActionState, VM: ScopedViewModel<U
         super.onCreate(savedInstanceState)
 
         viewModel.model.observe(this, Observer(::render))
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        context?.let {
+            view.background = ContextCompat.getDrawable(it, R.color.colorPrimary)
+        }
     }
 
     abstract fun render(state: UI)
